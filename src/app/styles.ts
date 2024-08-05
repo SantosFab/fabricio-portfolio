746ae2271e38e990c05bd1dd7fd545f6d8a8940a
SantosFab <fabriciosantos.dev@gmail.com>
2024-08-05 15:39:24 -0300

    feat: styled component

---
+++ src/app/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+
+export const Test = styled.div`
+  ${({ theme }) =>
+    css`
+      background-color: ${theme.colors.primary};
+    `}
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+interface ITheme {
+  colors: {
+    primary: string;
+    secondary: string;
+  };
+}
+
+export const theme: ITheme = {
+  colors: {
+    primary: "red",
+    secondary: "blue",
+  },
+};
