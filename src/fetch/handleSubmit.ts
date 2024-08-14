0bb9f7d22cc6cb08a9690afebf77dfa6529b7777
SantosFab <fabriciosantos.dev@gmail.com>
2024-08-14 13:09:56 -0300

    feat: implement email sending logic for form

---
+++ src/fetch/handleSubmit.ts
@@ -0,0 +1,37 @@
+"use client";
+import axios from "axios";
+
+interface IHandleSubmitProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  message: string;
+}
+
+export const handleSubmit = async ({
+  firstName,
+  lastName,
+  email,
+  phone,
+  message,
+}: IHandleSubmitProps) => {
+  try {
+    const response = await axios.post("/api/send-email", {
+      firstName,
+      lastName,
+      email,
+      phone,
+      message,
+    });
+
+    if (response.data.success) {
+      alert("Email enviado com sucesso!");
+    } else {
+      alert("Falha ao enviar o email.");
+    }
+  } catch (error) {
+    console.error("Erro ao enviar o email:", error);
+    alert("Ocorreu um erro ao enviar o email.");
+  }
+};
